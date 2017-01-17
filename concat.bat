82150c57695eb63839e267619ab307d5a03f4d49
Tokutaro Shimada <tokutaro@gmail.com>
2017-01-17 21:40:47 +0900

    add more eclipse plugin Rinzo, XMLEspresso, JavaCC

--- concat.bat
+++ concat.bat
@@ -1,2 +1,2 @@
 cd %~dp0
-copy /b eclipse.jar.001+eclipse.jar.002+eclipse.jar.003+eclipse.jar.004 eclipse.zip
+copy /b eclipse.jar.001+eclipse.jar.002+eclipse.jar.003+eclipse.jar.004+eclipse.jar.005 eclipse.zip
